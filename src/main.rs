f67b5fab6909e7976045946946c235eeff9e7556
Max Foxie <altfoxie@icloud.com>
2023-11-28 20:39:57 +0300

    skill issue

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{
+    extract::DefaultBodyLimit,
     routing::{get, post},
     Router, Server,
 };
@@ -10,8 +11,8 @@ mod container;
 mod handlers;
 mod templates;
 
-const PATH: &str = "ushlepok.json";
-const IMAGE_PATH: &str = "image.jpeg";
+const PATH: &str = "data/ushlepok.json";
+const IMAGE_PATH: &str = "data/image.jpeg";
 
 #[derive(Clone)]
 pub struct AppState {
@@ -21,6 +22,8 @@ pub struct AppState {
 
 #[tokio::main]
 async fn main() {
+    std::fs::create_dir_all("data").unwrap();
+
     let hbs = templates::init_handlebars().unwrap();
     let container = Container::new(std::path::Path::new(PATH), std::path::Path::new(IMAGE_PATH));
     if let Err(e) = container.load() {
@@ -34,6 +37,7 @@ async fn main() {
         .route("/add", post(handlers::add::post))
         .route("/rules", get(handlers::rules::get))
         .fallback(get(handlers::assets::get))
+        .layer(DefaultBodyLimit::max(1024 * 1024 * 10)) // 10 MB, TODO: only for /add
         .with_state(AppState {
             engine: Engine::from(hbs),
             container,
